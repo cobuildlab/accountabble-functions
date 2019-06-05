c6ccad88154d5ee5ffcc43fdf9b7d531381b57f1
anderjs <andersonav37@gmail.com>
2019-06-05 12:45:35 -0400

    Updated payment

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const Stripe = require("stripe");
+const config = require("./config");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.createPaymentRequest = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  const stripe = new Stripe(config.STRIPE_API_KEY);
+  try {
+    let { status } = await stripe.charges.create({
+      amount: 1500,
+      currency: "usd",
+      description: "Accountabble Membership",
+      source: req.body
+    });
+    return res.json({ status });
+  } catch (err) {
+    return res
+      .json({
+        statusCode: 500,
+        details: "Payment Request Failed"
+      })
+      .end();
+  }
+});
