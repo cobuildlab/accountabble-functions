1384fd0fb137234b534399642db196072f0e2219
anderjs <andersonav37@gmail.com>
2019-06-06 10:54:32 -0400

    Cors

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,30 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const Stripe = require("stripe");
+const cors = require("cors");
+
 const config = require("./config");
 
 admin.initializeApp();
 
-exports.createPaymentRequest = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  const stripe = new Stripe(config.STRIPE_API_KEY);
-  try {
-    let { status } = await stripe.charges.create({
-      amount: 1500,
-      currency: "usd",
-      description: "Accountabble Membership",
-      source: req.body
-    });
-    return res.json({ status });
-  } catch (err) {
-    return res
-      .json({
-        statusCode: 500,
-        details: "Payment Request Failed"
-      })
-      .end();
-  }
+exports.createPaymentRequest = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const stripe = new Stripe(config.STRIPE_API_KEY);
+    try {
+      let { status } = await stripe.charges.create({
+        amount: 1500,
+        currency: "usd",
+        description: "Accountabble Membership",
+        source: req.body
+      });
+      return res.json({ status });
+    } catch (err) {
+      return res
+        .json({
+          statusCode: 500,
+          details: "Payment Request Failed"
+        })
+        .end();
+    }
+  });
 });
