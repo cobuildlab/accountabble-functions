542ef09b9d49e8d53ecb6122e4624924fc7e3f45
Angel Lacret <alacret@gmail.com>
2019-06-13 00:09:52 -0400

    tester function

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,31 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const Stripe = require("stripe");
-
 const config = require("./config");
 
 admin.initializeApp();
 
 exports.createPaymentRequest = functions.https.onCall(
-  async (data, contenxt) => {
+  async (data) => {
     const token = data.token;
-    const stripe = new Stripe(config.STRIPE_API_KEY);
-    try {
-      let { status } = await stripe.charges.create({
-        amount: 1500,
-        currency: "usd",
-        description: "Accountabble Membership",
-        source: token
-      });
-      return {
-        payment: status
-      };
-    } catch (err) {
-      return {
-        err: err,
-        statusCode: 500,
-        details: "Payment Request Failed"
-      };
-    }
+    console.log(`DEBUG:`, data);
+    // const stripe = new Stripe(config.STRIPE_API_KEY);
+    // try {
+    //   let {status} = await stripe.charges.create({
+    //     amount: 1500,
+    //     currency: "usd",
+    //     description: "Accountabble Membership",
+    //     source: token
+    //   });
+    //   return {
+    //     payment: status
+    //   };
+    // } catch (err) {
+    //   return {
+    //     err: err,
+    //     statusCode: 500,
+    //     details: "Payment Request Failed"
+    //   };
+    // }
   }
 );
